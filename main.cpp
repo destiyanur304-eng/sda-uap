1d7992b087cc40551ca9c486fcfc891bfb7d8c25
farhatfatih05-glitch <farhatfatih05@gmail.com>
2026-06-10 10:50:51 +0700

    merubah main.cpp

--- main.cpp
+++ main.cpp
@@ -1,49 +1,9 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include "MenuUI.h" 
 
-struct pasien
-{
-    /* data */
-};
-
-class dataLengkap{
-    protected:
-    double berat, tinggi, imt;
+int main() {
+    MenuAplikasi aplikasi;
     
-    public:
-    double tinggiM = tinggi / 100;
-    imt = berat / (tinggiM*tinggiM);
+    aplikasi.jalankanProgram();
 
-    void hasilIMT(){
-    if (imt < 18.5){ 
-            cout << "Kategori: Kekurangan berat badan\n";
-        }else if (imt < 25){
-            cout << "Kategori: Normal\n";
-        }else if (imt < 30){
-            cout << "Kategori: Kelebihan berat badan\n";
-        }else{
-            cout << "Kategori: Obesitas\n";
-        } 
-    }
-};
-
-class menuBMR : public menuIMT {
-    public:
-    double bmr;
-    void hasilBMR(){
-        if (imt < 18.5){ 
-            bmr = 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * 20);
-        }else if (imt < 25){
-            bmr = 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * 20);
-        }else if (imt < 30){
-            bmr = 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * 20);
-        }else{
-            bmr = 655 + (9.6 * berat) + (1.8 * tinggi) - (4.7 * 20);
-        }
-    }
-};
-
-int main(){
-    
+    return 0;
 }
